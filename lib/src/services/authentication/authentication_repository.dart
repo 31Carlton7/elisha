660d8c4209ba736b985c4cbfa9c3af85211ab48e
Carlton Aikins <carltonaikins7@gmail.com>
2021-12-05 13:54:14 -0500

    Update ../lib/src/services/authentication/authentication_repository.dart

--- lib/src/services/authentication/authentication_repository.dart
+++ lib/src/services/authentication/authentication_repository.dart
@@ -1,24 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:elisha/src/config/authentication_exceptions.dart';
-import 'package:elisha/src/models/user.dart' as elisha;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+
+import 'package:elisha/src/config/authentication_exceptions.dart';
+import 'package:elisha/src/models/user.dart' as elisha;
 
 class AuthenticationRepository {
   final FirebaseAuth _firebaseAuth;
   AuthenticationRepository(this._firebaseAuth);
 
-  final CollectionReference userRef =
-      FirebaseFirestore.instance.collection('users');
+  final CollectionReference userRef = FirebaseFirestore.instance.collection('users');
 
   Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
 
   Future<void> _createUserInDatabase(elisha.User user) async {
-    await userRef.doc(user.id).set(user.toDocumentSnapshot());
+    await userRef.doc(FirebaseAuth.instance.currentUser?.uid).set(user.toDocumentSnapshot());
   }
 
-  Future<String> signInWithEmailAndPassword(
-      {required String email, required String password}) async {
+  Future<String> signInWithEmailAndPassword({required String email, required String password}) async {
     try {
       await _firebaseAuth.signInWithEmailAndPassword(
         email: email,
@@ -36,7 +35,12 @@ class AuthenticationRepository {
 
   Future<String> signInWithGoogle() async {
     try {
-      final googleUser = await GoogleSignIn().signIn();
+      final googleUser = await GoogleSignIn(
+        scopes: [
+          'email',
+          'https://www.googleapis.com/auth/user.birthday.read',
+        ],
+      ).signIn();
       if (googleUser == null) return '';
 
       final googleAuth = await googleUser.authentication;
@@ -46,14 +50,29 @@ class AuthenticationRepository {
         idToken: googleAuth.idToken,
       );
 
-      await FirebaseAuth.instance.signInWithCredential(googleCredential);
+      var currentUser = await FirebaseAuth.instance.signInWithCredential(googleCredential);
+      var res = await userRef.doc(FirebaseAuth.instance.currentUser?.uid).get();
+
+      if (!res.exists) {
+        var user = elisha.User(
+          firstName: currentUser.user?.displayName,
+          lastName: '',
+          email: currentUser.user?.email,
+          birthDate: currentUser.additionalUserInfo?.profile![''],
+          imageUrl: '',
+          currentStreak: 1,
+          bestStreak: 1,
+          perfectWeeks: 0,
+        );
+
+        _createUserInDatabase(user);
+      }
 
       return 'success';
     } catch (e) {
       if (e is FirebaseAuthException) {
         return AuthenticationExceptions.fromFirebaseAuthError(e).toString();
       } else if (e is RangeError) {
-        print(e);
         return '';
       }
       return 'failed';
@@ -73,10 +92,7 @@ class AuthenticationRepository {
         password: password,
       );
 
-      final uid = FirebaseAuth.instance.currentUser?.uid;
-
       var user = elisha.User(
-        id: uid,
         firstName: firstName,
         lastName: lastName,
         email: email,
