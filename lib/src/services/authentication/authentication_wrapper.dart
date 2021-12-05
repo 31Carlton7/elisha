3894f16316012a9beecdca890fefa0c1eb6ab469
Carlton Aikins <carltonaikins7@gmail.com>
2021-12-05 13:54:12 -0500

    Update ../lib/src/services/authentication/authentication_wrapper.dart

--- lib/src/services/authentication/authentication_wrapper.dart
+++ lib/src/services/authentication/authentication_wrapper.dart
@@ -2,13 +2,15 @@ import 'package:canton_design_system/canton_design_system.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:elisha/src/providers/authentication_providers/authentication_stream_provider.dart';
 import 'package:elisha/src/ui/components/something_went_wrong.dart';
-import 'package:elisha/src/ui/providers/authentication_providers/authentication_stream_provider.dart';
-import 'package:elisha/src/ui/views/authentication_views/sign_in_view.dart';
-import 'package:elisha/src/ui/views/authentication_views/sign_up_view.dart';
+import 'package:elisha/src/ui/views/authentication_views/sign_in_providers_view/sign_in_providers_view.dart';
+import 'package:elisha/src/ui/views/authentication_views/sign_up_view/sign_up_view.dart';
 import 'package:elisha/src/ui/views/current_view.dart';
 
 class AuthenticationWrapper extends StatefulWidget {
+  const AuthenticationWrapper({Key? key}) : super(key: key);
+
   @override
   _AuthenticationWrapperState createState() => _AuthenticationWrapperState();
 }
@@ -19,10 +21,10 @@ class _AuthenticationWrapperState extends State<AuthenticationWrapper> {
   @override
   void initState() {
     super.initState();
-    useLocalEmulator();
   }
 
-  void useLocalEmulator() {
+  // ignore: unused_element
+  void _useLocalEmulator() {
     var host = 'localhost';
     var firestorePort = 8080;
 
@@ -43,18 +45,18 @@ class _AuthenticationWrapperState extends State<AuthenticationWrapper> {
 
         return _authState.when(
           error: (e, s) {
-            return SomethingWentWrong();
+            return const SomethingWentWrong();
           },
           loading: () => Loading(),
           data: (user) {
             if (user == null) {
               if (showSignIn) {
-                return SignInView(toggleView);
+                return SignInProvidersView(toggleView);
               } else {
                 return SignUpView(toggleView);
               }
             } else {
-              return CurrentView();
+              return const CurrentView();
             }
           },
         );
