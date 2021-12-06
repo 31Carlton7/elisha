28206b6b201894d3886982055bb1f3a0bf95806f
Carlton Aikins <carltonaikins7@gmail.com>
2021-12-06 11:52:31 -0500

    Update ../lib/src/ui/views/home_view/home_view.dart

--- lib/src/ui/views/home_view/home_view.dart
+++ lib/src/ui/views/home_view/home_view.dart
@@ -1,9 +1,9 @@
 import 'package:canton_design_system/canton_design_system.dart';
+import 'package:elisha/src/providers/streaks_repository_provider.dart';
+import 'package:elisha/src/ui/views/home_view/components/home_view_header.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:elisha/src/models/user.dart';
-import 'package:elisha/src/providers/user_stream_provider.dart';
-import 'package:elisha/src/ui/components/something_went_wrong.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -20,75 +20,114 @@ class _HomeViewState extends State<HomeView> {
 
   Widget _content(BuildContext context) {
     return Consumer(builder: (context, watch, child) {
-      final userStreamRepo = watch(userStreamProvider);
-
-      return userStreamRepo.when(
-        error: (e, s) {
-          return const SomethingWentWrong();
-        },
-        loading: () => Loading(),
-        data: (user) {
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              _header(context, user),
-              _body(context),
-            ],
-          );
-        },
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const HomeViewHeader(),
+          _body(context),
+        ],
       );
     });
   }
 
-  Widget _header(BuildContext context, User user) {
-    String greeting() {
-      var hour = DateTime.now().hour;
-      if (hour < 12) {
-        return 'Morning';
-      }
-      if (hour < 17) {
-        return 'Afternoon';
-      }
-      return 'Evening';
-    }
-
-    String name(String source) {
-      if (source.length > 18) {
-        return source.substring(0, 15) + '...';
-      }
-      return source;
-    }
-
-    return Container(
-      padding: const EdgeInsets.symmetric(vertical: 25),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                'Good ' + greeting(),
-                style: Theme.of(context).textTheme.headline6?.copyWith(
-                      color: Theme.of(context).colorScheme.secondaryVariant,
-                    ),
-              ),
-              Text(
-                name(user.firstName!),
-                style: Theme.of(context).textTheme.headline2?.copyWith(
-                      fontWeight: FontWeight.w700,
-                    ),
-              ),
-            ],
-          ),
-        ],
-      ),
+  Widget _body(BuildContext context) {
+    return Column(
+      children: [
+        _streaksCard(),
+      ],
     );
   }
 
-  Widget _body(BuildContext context) {
-    return Column(
-      children: const [],
-    );
+  Widget _streaksCard() {
+    const divWidth = 30.0;
+
+    return Consumer(builder: (context, watch, child) {
+      final streaksRepo = watch(streaksRepositoryProvider);
+
+      final currentStreak = streaksRepo.currentStreak.toString();
+      final bestStreak = streaksRepo.bestStreak.toString();
+      final perfectWeeks = streaksRepo.perfectWeeks.toString();
+
+      return Card(
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: IntrinsicHeight(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Column(
+                    children: [
+                      FittedBox(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(currentStreak, style: Theme.of(context).textTheme.headline2),
+                            Icon(LineAwesomeIcons.fire, size: 27, color: Theme.of(context).primaryColor),
+                          ],
+                        ),
+                      ),
+                      Text(
+                        'Streak',
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText1
+                            ?.copyWith(color: Theme.of(context).colorScheme.secondaryVariant),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 35, child: VerticalDivider(width: divWidth)),
+                Expanded(
+                  child: Column(
+                    children: [
+                      FittedBox(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(bestStreak, style: Theme.of(context).textTheme.headline2),
+                            Icon(LineAwesomeIcons.star, size: 27, color: Theme.of(context).primaryColor),
+                          ],
+                        ),
+                      ),
+                      Text(
+                        'Best Streak',
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText1
+                            ?.copyWith(color: Theme.of(context).colorScheme.secondaryVariant),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 35, child: VerticalDivider(width: divWidth)),
+                Expanded(
+                  child: Column(
+                    children: [
+                      FittedBox(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(perfectWeeks, style: Theme.of(context).textTheme.headline2),
+                            Icon(LineAwesomeIcons.calendar, size: 27, color: Theme.of(context).primaryColor),
+                          ],
+                        ),
+                      ),
+                      Text(
+                        'Perfect Weeks',
+                        style: Theme.of(context)
+                            .textTheme
+                            .bodyText1
+                            ?.copyWith(color: Theme.of(context).colorScheme.secondaryVariant),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    });
   }
 }
