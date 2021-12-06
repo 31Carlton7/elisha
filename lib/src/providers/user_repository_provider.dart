ab93de0759ac4341a6d46dd56d813b72a47ade5d
Carlton Aikins <carltonaikins7@gmail.com>
2021-12-06 11:54:56 -0500

    Remove and update files

--- lib/src/providers/user_repository_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'package:elisha/src/repositories/user_repository.dart';
-
-final userRepositoryProvider = Provider<UserRepository>((ref) {
-  return UserRepository();
-});
