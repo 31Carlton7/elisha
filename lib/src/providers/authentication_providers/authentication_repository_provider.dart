4367328b0de754fa4d6f93113e3639511e3d2407
Carlton Aikins <carltonaikins7@gmail.com>
2021-12-06 11:53:13 -0500

    Update ../lib/src/providers/authentication_providers/authentication_repository_provider.dart

--- lib/src/providers/authentication_providers/authentication_repository_provider.dart
+++ lib/src/providers/authentication_providers/authentication_repository_provider.dart
@@ -1,7 +1,7 @@
+import 'package:elisha/src/services/authentication_services/authentication_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:elisha/src/providers/authentication_providers/firebase_auth_provider.dart';
-import 'package:elisha/src/services/authentication/authentication_repository.dart';
 
 final authenticationRepositoryProvider = Provider<AuthenticationRepository>((ref) {
   return AuthenticationRepository(ref.read(firebaseAuthProvider));
