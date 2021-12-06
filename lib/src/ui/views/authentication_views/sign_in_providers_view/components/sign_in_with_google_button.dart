578789ffbe11b86781047a3ac73afdf7e1861958
Carlton Aikins <carltonaikins7@gmail.com>
2021-12-06 12:02:31 -0500

    Update ../lib/src/ui/views/authentication_views/sign_in_providers_view/components/sign_in_with_google_button.dart

--- lib/src/ui/views/authentication_views/sign_in_providers_view/components/sign_in_with_google_button.dart
+++ lib/src/ui/views/authentication_views/sign_in_providers_view/components/sign_in_with_google_button.dart
@@ -16,10 +16,12 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import 'package:canton_design_system/canton_design_system.dart';
-import 'package:elisha/src/providers/authentication_providers/authentication_repository_provider.dart';
 import 'package:flutter/services.dart';
+
+import 'package:canton_design_system/canton_design_system.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:elisha/src/providers/authentication_providers/authentication_repository_provider.dart';
 
 class SignInWithGoogleButton extends StatelessWidget {
   const SignInWithGoogleButton({Key? key}) : super(key: key);
