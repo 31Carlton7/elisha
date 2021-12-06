8c4c9c384c4c6943f1406bdd00371ff9e62e2b37
Carlton Aikins <carltonaikins7@gmail.com>
2021-12-06 12:03:31 -0500

    Update ../lib/src/providers/local_user_repository_provider.dart

--- lib/src/providers/local_user_repository_provider.dart
+++ lib/src/providers/local_user_repository_provider.dart
@@ -16,10 +16,11 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:elisha/src/models/local_user.dart';
 import 'package:elisha/src/providers/authentication_providers/authentication_stream_provider.dart';
 import 'package:elisha/src/repositories/local_user_repository.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final localUserRepositoryProvider = StateNotifierProvider.autoDispose<LocalUserRepository, LocalUser>((ref) {
   ref.watch(authenticationStreamProvider).whenData((value) {
