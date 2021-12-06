40f4f06990e861419c67a65200f0a2ba32721dae
Carlton Aikins <carltonaikins7@gmail.com>
2021-12-06 11:57:46 -0500

    Update ../lib/src/ui/views/authentication_views/sign_in_view/sign_in_view.dart

--- lib/src/ui/views/authentication_views/sign_in_view/sign_in_view.dart
+++ lib/src/ui/views/authentication_views/sign_in_view/sign_in_view.dart
@@ -1,3 +1,21 @@
+/*
+Elisha iOS & Android App
+Copyright (C) 2021 Elisha
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+ any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import 'package:elisha/src/ui/components/terms_and_privacy_policy_text.dart';
 import 'package:elisha/src/ui/views/authentication_views/components/dont_have_an_account_text.dart';
 import 'package:flutter/services.dart';
