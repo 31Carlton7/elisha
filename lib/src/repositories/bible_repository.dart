4c714220e2b3f0f37f60c8cc9b38ce0f5b778a31
Carlton Aikins <carltonaikins7@gmail.com>
2021-12-13 12:28:00 -0500

    Update ../lib/src/repositories/bible_repository.dart

--- lib/src/repositories/bible_repository.dart
+++ lib/src/repositories/bible_repository.dart
@@ -16,6 +16,8 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+import 'package:elisha/src/models/book.dart';
+import 'package:elisha/src/models/chapter.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -26,6 +28,75 @@ import 'package:elisha/src/providers/last_translation_book_chapter_provider.dart
 
 class BibleRepository {
   BibleRepository();
+
+  final _mapOfBibleBooks = <int, String>{
+    1: 'Genesis',
+    2: 'Exodus',
+    3: 'Leviticus',
+    4: 'Numbers',
+    5: 'Deuteronomy',
+    6: 'Joshua',
+    7: 'Judges',
+    8: 'Ruth',
+    9: '1 Samuel',
+    10: '2 Samuel',
+    11: '1 Kings',
+    12: '2 Kings',
+    13: '1 Chronicles',
+    14: '2 Chronicles',
+    15: 'Ezra',
+    16: 'Nehemiah',
+    17: 'Esther',
+    18: 'Job',
+    19: 'Psalms',
+    20: 'Proverbs',
+    21: 'Ecclesiates',
+    22: 'Song of Solomon',
+    23: 'Isaiah',
+    24: 'Jeremiah',
+    25: 'Lamentations',
+    26: 'Ezekiel',
+    27: 'Daniel',
+    28: 'Hosea',
+    29: 'Joel',
+    30: 'Amos',
+    31: 'Obadiah',
+    32: 'Jonah',
+    33: 'Micah',
+    34: 'Nahum',
+    35: 'Habakkuk',
+    36: 'Zephaniah',
+    37: 'Haggai',
+    38: 'Zechariah',
+    39: 'Malachi',
+    40: 'Matthew',
+    41: 'Mark',
+    42: 'Luke',
+    43: 'John',
+    44: 'Acts',
+    45: 'Romans',
+    46: '1 Corinthians',
+    47: '2 Corinthians',
+    48: 'Galatians',
+    49: 'Ephesians',
+    50: 'Philippians',
+    51: 'Colossians',
+    52: '1 Thessalonians',
+    53: '2 Thessalonians',
+    54: '1 Timothy',
+    55: '2 Timothy',
+    56: 'Titus',
+    57: 'Philemon',
+    58: 'Hebrews',
+    59: 'James',
+    60: '1 Peter',
+    61: '2 Peter',
+    62: '1 John',
+    63: '2 John',
+    64: '3 John',
+    65: 'Jude',
+    66: 'Revelation',
+  };
 
   final Map<int, int> _mapOfBibleChaptersAndBooks = {
     0: 1,
@@ -99,6 +170,30 @@ class BibleRepository {
   };
 
   Map<int, int> get mapOfBibleChaptersAndBooks => _mapOfBibleChaptersAndBooks;
+
+  List<Book> getBooks() {
+    var books = <Book>[];
+
+    for (var item in _mapOfBibleBooks.entries.toList()) {
+      var newMap = _mapOfBibleChaptersAndBooks;
+
+      newMap.remove(0);
+      newMap.remove(67);
+
+      String testament() {
+        if (item.key < 40) return 'Old';
+        return 'New';
+      }
+
+      final chapters = List<ChapterId>.generate(newMap[item.key]!, (i) => ChapterId(id: i + 1));
+
+      final book = Book(id: item.key, chapters: chapters, name: item.value, testament: testament());
+
+      books.add(book);
+    }
+
+    return books;
+  }
 
   bool _isLastChapterInBook(BuildContext context) {
     bool bookChapterBool(int book, int chapter) {
