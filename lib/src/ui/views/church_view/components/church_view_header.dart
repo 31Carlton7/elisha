cea5fcadc4c65e061a6aad5b7ca4a941e400adc0
Carlton Aikins <carltonaikins7@gmail.com>
2021-12-20 22:59:03 -0500

    Update ../lib/src/ui/views/church_view/components/church_view_header.dart

---
+++ lib/src/ui/views/church_view/components/church_view_header.dart
@@ -0,0 +1,13 @@
+import 'package:canton_design_system/canton_design_system.dart';
+
+class ChurchViewHeader extends StatelessWidget {
+  const ChurchViewHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 25),
+      child: Row(children: [Text('Church', style: Theme.of(context).textTheme.headline2)]),
+    );
+  }
+}
