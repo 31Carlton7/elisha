b1fefe9b50ca649107ef755adbdf2a7557a2c050
Carlton Aikins <carltonaikins7@gmail.com>
2021-12-22 00:24:15 -0500

    Update ../lib/src/ui/views/authentication_views/verify_email_view/components/verify_email_header.dart

---
+++ lib/src/ui/views/authentication_views/verify_email_view/components/verify_email_header.dart
@@ -0,0 +1,14 @@
+import 'package:canton_design_system/canton_design_system.dart';
+
+class VerifyEmailHeader extends StatelessWidget {
+  const VerifyEmailHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: const [
+        CantonBackButton(isClear: true),
+      ],
+    );
+  }
+}
