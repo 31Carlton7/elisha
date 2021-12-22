34a6891dfee786fab5d03d5613fb50a42c8fa5d5
Carlton Aikins <carltonaikins7@gmail.com>
2021-12-22 00:24:41 -0500

    Update ../lib/src/providers/verse_of_the_day_future_provider.dart

---
+++ lib/src/providers/verse_of_the_day_future_provider.dart
@@ -0,0 +1,8 @@
+import 'package:elisha/src/models/verse.dart';
+import 'package:elisha/src/providers/verse_of_the_day_service_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final verseOfTheDayFutureProvider = FutureProvider.autoDispose<Verse>((ref) {
+  ref.maintainState = true;
+  return ref.read(verseOfTheDayServiceProvider).getVerseOfTheDay;
+});
