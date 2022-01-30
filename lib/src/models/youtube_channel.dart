9c1bd37204548a3ed00f213f27048ded8936e83e
Carlton Aikins <carltonaikins7@gmail.com>
2022-01-30 15:28:16 -0500

    Update ../lib/src/models/youtube_channel.dart

---
+++ lib/src/models/youtube_channel.dart
@@ -0,0 +1,32 @@
+import 'package:elisha/src/models/youtube_video.dart';
+
+class YouTubeChannel {
+  final String id;
+  final String title;
+  final String profilePictureUrl;
+  final String subscriberCount;
+  final String videoCount;
+  final String uploadPlaylistId;
+  List<YouTubeVideo>? videos;
+
+  YouTubeChannel({
+    required this.id,
+    required this.title,
+    required this.profilePictureUrl,
+    required this.subscriberCount,
+    required this.videoCount,
+    required this.uploadPlaylistId,
+    this.videos,
+  });
+
+  factory YouTubeChannel.fromMap(Map<String, dynamic> map) {
+    return YouTubeChannel(
+      id: map['id'],
+      title: map['snippet']['title'],
+      profilePictureUrl: map['snippet']['thumbnails']['default']['url'],
+      subscriberCount: map['statistics']['subscriberCount'],
+      videoCount: map['statistics']['videoCount'],
+      uploadPlaylistId: map['contentDetails']['relatedPlaylists']['uploads'],
+    );
+  }
+}
