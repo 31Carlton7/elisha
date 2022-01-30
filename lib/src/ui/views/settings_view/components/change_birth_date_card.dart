0a42fc6c7be5f71f793d3637a23090f59c2595d6
Carlton Aikins <carltonaikins7@gmail.com>
2022-01-30 15:27:01 -0500

    Update ../lib/src/ui/views/settings_view/components/change_birth_date_card.dart

---
+++ lib/src/ui/views/settings_view/components/change_birth_date_card.dart
@@ -0,0 +1,103 @@
+import 'dart:io';
+
+import 'package:canton_design_system/canton_design_system.dart';
+import 'package:elisha/src/providers/local_user_repository_provider.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
+
+class ChangeBirthDateCard extends StatelessWidget {
+  const ChangeBirthDateCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var _controller = DateTime.now();
+    final initialDate = DateTime.now();
+    final maximumYear = DateTime.now().year;
+    final firstDate = DateTime(1900);
+    final lastDate = DateTime.now();
+    String currentBirthdayStr() {
+      return 'Current Birthday: ' +
+          DateFormat('yMMMd').format((context.read(localUserRepositoryProvider).getUser.birthDate));
+    }
+
+    return CantonExpansionTile(
+      title: Text(
+        'Change Birthday',
+        style: Theme.of(context).textTheme.headline6,
+      ),
+      decoration: BoxDecoration(
+        color: CantonMethods.alternateCanvasColorType2(context),
+        borderRadius: const BorderRadius.vertical(bottom: Radius.circular(10)),
+      ),
+      backgroundColor: CantonMethods.alternateCanvasColor(context),
+      iconColor: Theme.of(context).colorScheme.primary,
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(horizontal: 17, vertical: 17),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                currentBirthdayStr(),
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                height: 200,
+                child: Platform.isIOS
+                    ? CupertinoTheme(
+                        data: CupertinoThemeData(
+                          brightness: MediaQuery.of(context).platformBrightness,
+                        ),
+                        child: CupertinoDatePicker(
+                          mode: CupertinoDatePickerMode.date,
+                          initialDateTime: initialDate,
+                          minimumDate: firstDate,
+                          minimumYear: firstDate.year,
+                          maximumDate: initialDate,
+                          maximumYear: maximumYear,
+                          onDateTimeChanged: (date) {
+                            _controller = date;
+                          },
+                        ),
+                      )
+                    : DatePickerDialog(
+                        initialDate: initialDate,
+                        firstDate: firstDate,
+                        lastDate: lastDate,
+                        initialCalendarMode: DatePickerMode.day,
+                        selectableDayPredicate: (date) {
+                          _controller = date;
+                          return true;
+                        },
+                      ),
+              ),
+              const SizedBox(height: 17),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CantonPrimaryButton(
+                    buttonText: 'Confirm',
+                    color: Theme.of(context).colorScheme.primary,
+                    textColor: Theme.of(context).colorScheme.onBackground,
+                    containerWidth: 100,
+                    containerHeight: 30,
+                    padding: EdgeInsets.zero,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                    onPressed: () {
+                      final updatedUser =
+                          context.read(localUserRepositoryProvider).getUser.copyWith(birthDate: _controller);
+                      context.read(localUserRepositoryProvider).updateUser(updatedUser);
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
