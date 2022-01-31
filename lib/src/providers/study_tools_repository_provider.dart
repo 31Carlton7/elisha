7c7fad0a9eb6f4f810728cfacdaa00a6a5ff561d
Carlton Aikins <carltonaikins7@gmail.com>
2022-01-31 01:43:28 -0500

    Update ../lib/src/providers/study_tools_repository_provider.dart

--- lib/src/providers/study_tools_repository_provider.dart
+++ lib/src/providers/study_tools_repository_provider.dart
@@ -16,8 +16,9 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import 'package:elisha/src/repositories/study_tools_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:elisha/src/repositories/study_tools_repository.dart';
 
 final studyToolsRepositoryProvider = ChangeNotifierProvider<StudyToolsRepository>((ref) {
   return StudyToolsRepository();
