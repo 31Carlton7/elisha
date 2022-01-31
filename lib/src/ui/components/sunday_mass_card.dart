1dee4157269537b0001c697ecb615b746e1e1de2
Carlton Aikins <carltonaikins7@gmail.com>
2022-01-31 01:35:45 -0500

    Update ../lib/src/ui/components/sunday_mass_card.dart

--- lib/src/ui/components/sunday_mass_card.dart
+++ lib/src/ui/components/sunday_mass_card.dart
@@ -17,9 +17,10 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 import 'package:canton_design_system/canton_design_system.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:elisha/src/providers/local_user_repository_provider.dart';
 import 'package:elisha/src/ui/views/sunday_mass_view/sunday_mass_view.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class SundayMassCard extends ConsumerWidget {
   const SundayMassCard({Key? key}) : super(key: key);
