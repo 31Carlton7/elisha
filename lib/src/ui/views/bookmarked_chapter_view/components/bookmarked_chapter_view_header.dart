a8948e5636f6472d5214380e7a00931cfc32679a
Carlton Aikins <carltonaikins7@gmail.com>
2022-01-31 01:43:05 -0500

    Update ../lib/src/ui/views/bookmarked_chapter_view/components/bookmarked_chapter_view_header.dart

--- lib/src/ui/views/bookmarked_chapter_view/components/bookmarked_chapter_view_header.dart
+++ lib/src/ui/views/bookmarked_chapter_view/components/bookmarked_chapter_view_header.dart
@@ -17,6 +17,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 import 'package:canton_design_system/canton_design_system.dart';
+
 import 'package:elisha/src/models/chapter.dart';
 
 class BookmarkedChapterViewHeader extends StatelessWidget {
