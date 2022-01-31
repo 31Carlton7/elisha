cb58beabf57bb81b0e9664de5d1e68624787086e
Carlton Aikins <carltonaikins7@gmail.com>
2022-01-31 01:43:29 -0500

    Update ../lib/src/providers/verse_of_the_day_service_provider.dart

--- lib/src/providers/verse_of_the_day_service_provider.dart
+++ lib/src/providers/verse_of_the_day_service_provider.dart
@@ -17,8 +17,9 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 import 'package:dio/dio.dart';
-import 'package:elisha/src/services/verse_of_the_day_service.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:elisha/src/services/verse_of_the_day_service.dart';
 
 final verseOfTheDayServiceProvider = Provider<VerseOfTheDayService>((ref) {
   return VerseOfTheDayService(Dio());
