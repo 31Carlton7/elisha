fa8d2920bc86df6f44c49ab12bdc15686657af88
Carlton Aikins <carltonaikins7@gmail.com>
2022-01-31 01:43:13 -0500

    Update ../lib/src/ui/views/login_wrapper.dart

--- lib/src/ui/views/login_wrapper.dart
+++ lib/src/ui/views/login_wrapper.dart
@@ -17,10 +17,11 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 import 'package:canton_design_system/canton_design_system.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:elisha/src/providers/local_user_repository_provider.dart';
 import 'package:elisha/src/ui/views/current_view.dart';
 import 'package:elisha/src/ui/views/introduction_view/introduction_view.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class LoginWrapper extends StatefulWidget {
   const LoginWrapper({Key? key}) : super(key: key);
