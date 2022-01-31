748db7cf678f3bb3ff3749b157482fc0c538b4ac
Carlton Aikins <carltonaikins7@gmail.com>
2022-01-31 01:16:55 -0500

    Update ../lib/src/models/reading.dart

--- lib/src/models/reading.dart
+++ lib/src/models/reading.dart
@@ -16,24 +16,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-/*
-Elisha iOS & Android App
-Copyright (C) 2021 Elisha
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
- any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
-
 import 'dart:convert';
 
 class Reading {
@@ -76,21 +58,16 @@ class Reading {
 
   String toJson() => json.encode(toMap());
 
-  factory Reading.fromJson(String source) =>
-      Reading.fromMap(json.decode(source));
+  factory Reading.fromJson(String source) => Reading.fromMap(json.decode(source));
 
   @override
-  String toString() =>
-      'Reading(name: $name, snippetAddress: $snippetAddress, text: $text)';
+  String toString() => 'Reading(name: $name, snippetAddress: $snippetAddress, text: $text)';
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
 
-    return other is Reading &&
-        other.name == name &&
-        other.snippetAddress == snippetAddress &&
-        other.text == text;
+    return other is Reading && other.name == name && other.snippetAddress == snippetAddress && other.text == text;
   }
 
   @override
