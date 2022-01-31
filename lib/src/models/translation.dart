c6658d59c0b686b9c6d293cb1d3a3c2c779d9963
Carlton Aikins <carltonaikins7@gmail.com>
2022-01-31 01:16:50 -0500

    Update ../lib/src/models/translation.dart

--- lib/src/models/translation.dart
+++ lib/src/models/translation.dart
@@ -16,24 +16,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-/*
-Elisha iOS & Android App
-Copyright (C) 2021 Elisha
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
- any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
-
 import 'dart:convert';
 
 class Translation {
@@ -83,8 +65,7 @@ class Translation {
 
   String toJson() => json.encode(toMap());
 
-  factory Translation.fromJson(String source) =>
-      Translation.fromMap(json.decode(source));
+  factory Translation.fromJson(String source) => Translation.fromMap(json.decode(source));
 
   @override
   String toString() {
@@ -104,9 +85,6 @@ class Translation {
 
   @override
   int get hashCode {
-    return id.hashCode ^
-        name.hashCode ^
-        abbreviation.hashCode ^
-        language.hashCode;
+    return id.hashCode ^ name.hashCode ^ abbreviation.hashCode ^ language.hashCode;
   }
 }
