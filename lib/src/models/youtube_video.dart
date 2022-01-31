d0106a27a047870fc558f44f1b816fcbe5488b1b
Carlton Aikins <carltonaikins7@gmail.com>
2022-01-31 01:14:10 -0500

    Update ../lib/src/models/youtube_video.dart

--- lib/src/models/youtube_video.dart
+++ lib/src/models/youtube_video.dart
@@ -1,3 +1,21 @@
+/*
+Elisha iOS & Android App
+Copyright (C) 2022 Carlton Aikins
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 class YouTubeVideo {
   final String id;
   final String title;
