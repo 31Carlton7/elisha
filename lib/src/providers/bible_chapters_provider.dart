abbc991e57ded10ef62712399a8260f751c1ad06
Carlton Aikins <carltonaikins7@gmail.com>
2022-02-02 23:27:48 -0500

    Update ../lib/src/providers/bible_chapters_provider.dart

--- lib/src/providers/bible_chapters_provider.dart
+++ lib/src/providers/bible_chapters_provider.dart
@@ -25,10 +25,10 @@ import 'package:elisha/src/providers/bible_service_provider.dart';
 String chapterID = '';
 String translationAbb = '';
 
-final bibleChaptersProvider = FutureProvider.autoDispose<Chapter>((ref) async {
+final bibleChaptersProvider = FutureProvider.autoDispose<Chapter>((ref) {
   ref.maintainState = true;
 
-  final bibleService = ref.read(bibleServiceProvider);
+  final bibleService = ref.watch(bibleServiceProvider);
   final chapters = bibleService.getChapter(bookID, chapterID, translationAbb);
 
   return chapters;
