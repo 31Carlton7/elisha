97058367f378e7c7bf34e4721daa528257b03b5e
Carlton Aikins <carltonaikins7@gmail.com>
2022-02-02 23:27:02 -0500

    Update ../lib/src/ui/views/profile_view/components/privacy_policy_card.dart

--- lib/src/ui/views/profile_view/components/privacy_policy_card.dart
+++ lib/src/ui/views/profile_view/components/privacy_policy_card.dart
@@ -17,15 +17,16 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 import 'package:canton_design_system/canton_design_system.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class PrivacyPolicyCard extends StatelessWidget {
+class PrivacyPolicyCard extends ConsumerWidget {
   const PrivacyPolicyCard({
     Key? key,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return GestureDetector(
       onTap: () async {
         const link = 'https://31carlton7.github.io/elisha/privacy_policy';
