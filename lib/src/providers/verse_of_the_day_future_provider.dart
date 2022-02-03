467d4971f28ab4852b32b4af6236c98abc46071b
Carlton Aikins <carltonaikins7@gmail.com>
2022-02-02 23:27:44 -0500

    Update ../lib/src/providers/verse_of_the_day_future_provider.dart

--- lib/src/providers/verse_of_the_day_future_provider.dart
+++ lib/src/providers/verse_of_the_day_future_provider.dart
@@ -23,5 +23,6 @@ import 'package:elisha/src/providers/verse_of_the_day_service_provider.dart';
 
 final verseOfTheDayFutureProvider = FutureProvider.autoDispose<List<Verse>>((ref) {
   ref.maintainState = true;
-  return ref.read(verseOfTheDayServiceProvider).getVerseOfTheDay;
+
+  return ref.watch(verseOfTheDayServiceProvider).getVerseOfTheDay;
 });
