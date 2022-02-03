21e72a43f9a642c60b025c8f34fe99ab151f9d7d
Carlton Aikins <carltonaikins7@gmail.com>
2022-02-02 23:26:46 -0500

    Update ../lib/src/ui/views/settings_view/components/settings_view_header.dart

--- lib/src/ui/views/settings_view/components/settings_view_header.dart
+++ lib/src/ui/views/settings_view/components/settings_view_header.dart
@@ -17,12 +17,13 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
 import 'package:canton_design_system/canton_design_system.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SettingsViewHeader extends StatelessWidget {
+class SettingsViewHeader extends ConsumerWidget {
   const SettingsViewHeader({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return const ViewHeaderTwo(
       title: 'Settings',
       backButton: true,
