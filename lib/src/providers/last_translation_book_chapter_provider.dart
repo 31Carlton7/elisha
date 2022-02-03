3f404f46e23c9234c10c6ecf0dcf7c284d1b3c1f
Carlton Aikins <carltonaikins7@gmail.com>
2022-02-02 23:27:52 -0500

    Update ../lib/src/providers/last_translation_book_chapter_provider.dart

--- lib/src/providers/last_translation_book_chapter_provider.dart
+++ lib/src/providers/last_translation_book_chapter_provider.dart
@@ -22,7 +22,6 @@ import 'package:elisha/src/models/translation_book_chapter.dart';
 import 'package:elisha/src/repositories/last_translation_book_chapter_repository.dart';
 
 final localRepositoryProvider =
-    StateNotifierProvider.autoDispose<LastTranslationBookChapterRepository, TranslationBookChapter>((ref) {
-  ref.maintainState = true;
+    StateNotifierProvider<LastTranslationBookChapterRepository, TranslationBookChapter>((ref) {
   return LastTranslationBookChapterRepository();
 });
