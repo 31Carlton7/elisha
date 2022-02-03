5e6e0dfbb34f6f3e296e1a973b31955589d30185
Carlton Aikins <carltonaikins7@gmail.com>
2022-02-03 00:14:46 -0500

    Update ../lib/src/ui/views/verse_of_the_day_view/verse_of_the_day_view.dart

--- lib/src/ui/views/verse_of_the_day_view/verse_of_the_day_view.dart
+++ lib/src/ui/views/verse_of_the_day_view/verse_of_the_day_view.dart
@@ -42,7 +42,7 @@ class _VerseOfTheDayViewState extends ConsumerState<VerseOfTheDayView> {
   @override
   void initState() {
     super.initState();
-    _isFavorite = ref.watch(studyToolsRepositoryProvider).favoriteVerses.where((e) {
+    _isFavorite = ref.read(studyToolsRepositoryProvider).favoriteVerses.where((e) {
       return widget.verses.any((element) => e.id == element.id);
     }).isNotEmpty;
   }
@@ -67,100 +67,96 @@ class _VerseOfTheDayViewState extends ConsumerState<VerseOfTheDayView> {
   }
 
   Widget _header(BuildContext context) {
-    return Consumer(
-      builder: (context, ref, child) {
-        Widget _favoriteButton(BuildContext context) {
-          Color heartColor() {
-            if (MediaQuery.of(context).platformBrightness == Brightness.dark) {
-              return CantonDarkColors.red[400]!;
-            }
-            return CantonColors.red[400]!;
-          }
-
-          Icon icon() {
-            if (ref.watch(studyToolsRepositoryProvider).favoriteVerses.where((e) {
-              return widget.verses.any((element) => e.id == element.id);
-            }).isNotEmpty) {
-              return Icon(LineAwesomeIcons.heart_1, size: 24, color: heartColor());
-            }
-            return Icon(LineAwesomeIcons.heart, size: 24, color: Theme.of(context).colorScheme.primary);
-          }
-
-          return GestureDetector(
-            onTap: () async {
-              HapticFeedback.mediumImpact();
-
-              setState(() {
-                _isFavorite = !_isFavorite;
-              });
-
-              for (Verse item in widget.verses) {
-                if (_isFavorite) {
-                  await ref.read(studyToolsRepositoryProvider).addFavoriteVerse(item);
-                } else {
-                  await ref.read(studyToolsRepositoryProvider).removeFavoriteVerse(item);
-                }
-              }
-            },
-            child: Container(
-              height: 40,
-              width: 40,
-              margin: const EdgeInsets.symmetric(horizontal: 17),
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: Theme.of(context).colorScheme.onBackground,
-              ),
-              child: Center(
-                child: icon(),
-              ),
-            ),
-          );
+    Widget _favoriteButton(BuildContext context) {
+      Color heartColor() {
+        if (MediaQuery.of(context).platformBrightness == Brightness.dark) {
+          return CantonDarkColors.red[400]!;
         }
+        return CantonColors.red[400]!;
+      }
 
-        return SliverAppBar(
-          expandedHeight: 250,
-          stretch: true,
-          floating: false,
-          pinned: true,
-          automaticallyImplyLeading: false,
-          leadingWidth: 56 + kDefaultPadding,
-          backgroundColor: CantonMethods.alternateCanvasColor(context),
-          leading: Container(
-            padding: const EdgeInsets.all(7),
-            child: CantonPrimaryButton(
-              color: Theme.of(context).colorScheme.onBackground,
-              containerHeight: 45.0,
-              containerWidth: 45.0,
-              shape: const CircleBorder(),
-              padding: EdgeInsets.zero,
-              iconPadding: 0,
-              prefixIcon: Icon(
-                FeatherIcons.arrowLeft,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-              alignment: MainAxisAlignment.center,
-              onPressed: () => Navigator.of(context).pop(),
-            ),
+      Icon icon() {
+        if (ref.watch(studyToolsRepositoryProvider).favoriteVerses.where((e) {
+          return widget.verses.any((element) => e.id == element.id);
+        }).isNotEmpty) {
+          return Icon(LineAwesomeIcons.heart_1, size: 24, color: heartColor());
+        }
+        return Icon(LineAwesomeIcons.heart, size: 24, color: Theme.of(context).colorScheme.primary);
+      }
+
+      return GestureDetector(
+        onTap: () async {
+          HapticFeedback.mediumImpact();
+
+          setState(() {
+            _isFavorite = !_isFavorite;
+          });
+
+          for (Verse item in widget.verses) {
+            if (_isFavorite) {
+              await ref.read(studyToolsRepositoryProvider).addFavoriteVerse(item);
+            } else {
+              await ref.read(studyToolsRepositoryProvider).removeFavoriteVerse(item);
+            }
+          }
+        },
+        child: Container(
+          height: 40,
+          width: 40,
+          margin: const EdgeInsets.symmetric(horizontal: 17),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: Theme.of(context).colorScheme.onBackground,
           ),
-          actions: [
-            _favoriteButton(context),
-          ],
-          flexibleSpace: FlexibleSpaceBar(
-            background: _buildImage(context),
-            centerTitle: true,
-            title: Padding(
-              padding: const EdgeInsets.symmetric(vertical: kSmallPadding),
-              child: Text(
-                'Verse of the Day',
-                style: Theme.of(context).textTheme.headline4?.copyWith(
-                      color: CantonColors.white,
-                      fontSize: 17,
-                    ),
-              ),
-            ),
+          child: Center(
+            child: icon(),
           ),
-        );
-      },
+        ),
+      );
+    }
+
+    return SliverAppBar(
+      expandedHeight: 250,
+      stretch: true,
+      floating: false,
+      pinned: true,
+      automaticallyImplyLeading: false,
+      leadingWidth: 56 + kDefaultPadding,
+      backgroundColor: CantonMethods.alternateCanvasColor(context),
+      leading: Container(
+        padding: const EdgeInsets.all(7),
+        child: CantonPrimaryButton(
+          color: Theme.of(context).colorScheme.onBackground,
+          containerHeight: 45.0,
+          containerWidth: 45.0,
+          shape: const CircleBorder(),
+          padding: EdgeInsets.zero,
+          iconPadding: 0,
+          prefixIcon: Icon(
+            FeatherIcons.arrowLeft,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+          alignment: MainAxisAlignment.center,
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+      ),
+      actions: [
+        _favoriteButton(context),
+      ],
+      flexibleSpace: FlexibleSpaceBar(
+        background: _buildImage(context),
+        centerTitle: true,
+        title: Padding(
+          padding: const EdgeInsets.symmetric(vertical: kSmallPadding),
+          child: Text(
+            'Verse of the Day',
+            style: Theme.of(context).textTheme.headline4?.copyWith(
+                  color: CantonColors.white,
+                  fontSize: 17,
+                ),
+          ),
+        ),
+      ),
     );
   }
 
@@ -173,71 +169,67 @@ class _VerseOfTheDayViewState extends ConsumerState<VerseOfTheDayView> {
   }
 
   Widget _body(BuildContext context) {
-    return Consumer(
-      builder: (context, ref, child) {
-        Widget _bookChapterVerse(BuildContext context) {
-          String versesString() {
-            var str = '';
-            if (widget.verses.length > 1) {
-              str = widget.verses.first.verseId.toString() + ' - ' + widget.verses.last.verseId.toString();
-            } else {
-              str = widget.verses[0].verseId.toString();
-            }
-
-            return str;
-          }
-
-          return SelectableText(
-            widget.verses[0].book.name! + ' ' + widget.verses[0].chapterId.toString() + ':' + versesString(),
-            style: Theme.of(context).textTheme.headline6?.copyWith(
-                  fontWeight: FontWeight.w500,
-                  height: ref.watch(readerSettingsRepositoryProvider.notifier).bodyTextHeight,
-                  fontFamily: ref.watch(readerSettingsRepositoryProvider).typeFace,
-                ),
-          );
+    Widget _bookChapterVerse(BuildContext context) {
+      String versesString() {
+        var str = '';
+        if (widget.verses.length > 1) {
+          str = widget.verses.first.verseId.toString() + ' - ' + widget.verses.last.verseId.toString();
+        } else {
+          str = widget.verses[0].verseId.toString();
         }
 
-        return SliverToBoxAdapter(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 27.0),
-            child: Column(
-              children: [
-                const SizedBox(height: kDefaultPadding),
-                for (var verse in widget.verses)
-                  ConstrainedBox(
-                    constraints: const BoxConstraints(maxWidth: 600),
-                    child: SelectableText.rich(
+        return str;
+      }
+
+      return SelectableText(
+        widget.verses[0].book.name! + ' ' + widget.verses[0].chapterId.toString() + ':' + versesString(),
+        style: Theme.of(context).textTheme.headline6?.copyWith(
+              fontWeight: FontWeight.w500,
+              height: ref.watch(readerSettingsRepositoryProvider.notifier).bodyTextHeight,
+              fontFamily: ref.watch(readerSettingsRepositoryProvider).typeFace,
+            ),
+      );
+    }
+
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 27.0),
+        child: Column(
+          children: [
+            const SizedBox(height: kDefaultPadding),
+            for (var verse in widget.verses)
+              ConstrainedBox(
+                constraints: const BoxConstraints(maxWidth: 600),
+                child: SelectableText.rich(
+                  TextSpan(
+                    text: verse.verseId.toString() + ' ',
+                    style: Theme.of(context).textTheme.bodyText1?.copyWith(
+                          color: Theme.of(context).colorScheme.secondaryVariant,
+                          fontSize: ref.watch(readerSettingsRepositoryProvider).verseNumberSize * 1.5,
+                          height: ref.watch(readerSettingsRepositoryProvider).verseNumberHeight,
+                          fontFamily: ref.watch(readerSettingsRepositoryProvider).typeFace,
+                        ),
+                    children: [
                       TextSpan(
-                        text: verse.verseId.toString() + ' ',
-                        style: Theme.of(context).textTheme.bodyText1?.copyWith(
-                              color: Theme.of(context).colorScheme.secondaryVariant,
-                              fontSize: ref.watch(readerSettingsRepositoryProvider).verseNumberSize * 1.5,
-                              height: ref.watch(readerSettingsRepositoryProvider).verseNumberHeight,
+                        text: (verse.text + (widget.verses.last == verse ? '' : ' ')),
+                        style: Theme.of(context).textTheme.headline5!.copyWith(
+                              fontWeight: FontWeight.w400,
+                              fontSize: ref.watch(readerSettingsRepositoryProvider.notifier).bodyTextSize * 1.5,
+                              height: ref.watch(readerSettingsRepositoryProvider.notifier).bodyTextHeight,
                               fontFamily: ref.watch(readerSettingsRepositoryProvider).typeFace,
                             ),
-                        children: [
-                          TextSpan(
-                            text: (verse.text + (widget.verses.last == verse ? '' : ' ')),
-                            style: Theme.of(context).textTheme.headline5!.copyWith(
-                                  fontWeight: FontWeight.w400,
-                                  fontSize: ref.watch(readerSettingsRepositoryProvider.notifier).bodyTextSize * 1.5,
-                                  height: ref.watch(readerSettingsRepositoryProvider.notifier).bodyTextHeight,
-                                  fontFamily: ref.watch(readerSettingsRepositoryProvider).typeFace,
-                                ),
-                          ),
-                        ],
                       ),
-                      cursorColor: Theme.of(context).primaryColor,
-                    ),
+                    ],
                   ),
-                const SizedBox(height: kDefaultPadding),
-                _bookChapterVerse(context),
-                const SizedBox(height: kDefaultPadding),
-              ],
-            ),
-          ),
-        );
-      },
+                  cursorColor: Theme.of(context).primaryColor,
+                ),
+              ),
+            const SizedBox(height: kDefaultPadding),
+            _bookChapterVerse(context),
+            const SizedBox(height: kDefaultPadding),
+          ],
+        ),
+      ),
     );
   }
 }
