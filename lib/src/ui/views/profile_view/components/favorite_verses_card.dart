47d616a9c2c89af497b730fd2bc28281f2424fb3
Carlton Aikins <carltonaikins7@gmail.com>
2022-02-02 23:27:11 -0500

    Update ../lib/src/ui/views/profile_view/components/favorite_verses_card.dart

--- lib/src/ui/views/profile_view/components/favorite_verses_card.dart
+++ lib/src/ui/views/profile_view/components/favorite_verses_card.dart
@@ -19,12 +19,13 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import 'package:canton_design_system/canton_design_system.dart';
 
 import 'package:elisha/src/ui/views/favorite_verses_view/favorite_verses_view.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoriteVersesCard extends StatelessWidget {
+class FavoriteVersesCard extends ConsumerWidget {
   const FavoriteVersesCard({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return GestureDetector(
       onTap: () {
         CantonMethods.viewTransition(context, const FavoriteVersesView());
