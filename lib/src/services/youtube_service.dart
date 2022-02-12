c9eafee70d635e84c3b0ade16d5f639232279240
Carlton Aikins <carltonaikins7@gmail.com>
2022-02-12 02:33:30 -0500

    Update ../lib/src/services/youtube_service.dart

--- lib/src/services/youtube_service.dart
+++ lib/src/services/youtube_service.dart
@@ -23,7 +23,8 @@ import 'package:dio/dio.dart';
 import 'package:elisha/src/config/keys.dart';
 import 'package:elisha/src/models/youtube_channel.dart';
 import 'package:elisha/src/models/youtube_video.dart';
-import 'package:youtube_explode_dart/youtube_explode_dart.dart';
+import 'package:elisha/src/providers/youtube_fetch_channel_future_provider.dart';
+import 'package:elisha/src/services/sunday_mass_service.dart';
 
 class YouTubeService {
   YouTubeService(this._dio);
@@ -90,16 +91,30 @@ class YouTubeService {
     }
   }
 
-  Future<String> getMP4Url(String videoId) async {
-    try {
-      final extractor = YoutubeExplode();
-      final streamManifest = await extractor.videos.streamsClient.getManifest(videoId);
-      final streamInfo = streamManifest.muxed.withHighestBitrate();
-      extractor.close();
+  Future<List<dynamic>> fetchLatestChurchVideoId() async {
+    final channel = await fetchChannel(YOUTUBE_CHANNEL_ID);
 
-      return streamInfo.url.toString();
-    } catch (e) {
-      rethrow;
+    final list = SundayMassService().getChurchYouTubeChannelIds;
+
+    if (list[0] == channel.id) {
+      return [channel.videos!.where((element) => element.title.contains('The Sunday Mass')).toList()[0].id, channel];
+    } else if (list[1] == channel.id) {
+      return [channel.videos![0].id, channel];
+    } else if (list[2] == channel.id) {
+      return [
+        channel.videos!.where((element) => element.title.contains('Cornerstone Church LIVE')).toList()[0].id,
+        channel,
+      ];
+    } else if (list[3] == channel.id) {
+      return [
+        channel.videos!
+            .where((element) => element.title.contains('Catholic Mass Today | Daily TV Mass'))
+            .toList()[0]
+            .id,
+        channel,
+      ];
+    } else {
+      return ['', channel];
     }
   }
 }
