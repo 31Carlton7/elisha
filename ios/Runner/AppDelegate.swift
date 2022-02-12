d82372497fb3d45ff3f86ffe70703111704b5ae2
Carlton Aikins <carltonaikins7@gmail.com>
2022-02-12 02:30:11 -0500

    Setup Google Ads

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,7 @@
 import UIKit
 import Flutter
 import Firebase
+import GoogleMobileAds
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -10,6 +11,7 @@ import Firebase
   ) -> Bool {
     FirebaseApp.configure()
     GeneratedPluginRegistrant.register(with: self)
+    GADMobileAds.sharedInstance().requestConfiguration.testDeviceIdentifiers = [ "da4cd32310d009a994d3d317e46037b0" ]
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
