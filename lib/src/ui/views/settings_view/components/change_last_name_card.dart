6726fba10b3a90802ee1e38c583562b8df03c18a
Carlton Aikins <carltonaikins7@gmail.com>
2022-02-26 23:47:38 -0500

    Update ../lib/src/ui/views/settings_view/components/change_last_name_card.dart

--- lib/src/ui/views/settings_view/components/change_last_name_card.dart
+++ lib/src/ui/views/settings_view/components/change_last_name_card.dart
@@ -38,7 +38,7 @@ class ChangeLastNameCard extends ConsumerWidget {
         style: Theme.of(context).textTheme.headline6,
       ),
       decoration: BoxDecoration(
-        color: CantonMethods.alternateCanvasColorType2(context),
+        color: CantonMethods.alternateCanvasColorType3(context),
         borderRadius: BorderRadius.zero,
       ),
       backgroundColor: CantonMethods.alternateCanvasColor(context),
