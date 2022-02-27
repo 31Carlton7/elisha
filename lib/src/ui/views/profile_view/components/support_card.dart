091fb03cf8e9d27d7c1a45a4eaeaccfb95872527
Carlton Aikins <carltonaikins7@gmail.com>
2022-02-26 23:47:58 -0500

    Update ../lib/src/ui/views/profile_view/components/support_card.dart

---
+++ lib/src/ui/views/profile_view/components/support_card.dart
@@ -0,0 +1,55 @@
+/*
+Elisha iOS & Android App
+Copyright (C) 2022 Carlton Aikins
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import 'package:canton_design_system/canton_design_system.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class SupportCard extends ConsumerWidget {
+  const SupportCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return GestureDetector(
+      onTap: () async {
+        const link = 'https://31carlton7.github.io/elisha/support';
+
+        if (await canLaunch(link)) {
+          await launch(link);
+        } else {
+          throw 'Could not launch $link';
+        }
+      },
+      child: Card(
+        color: CantonMethods.alternateCanvasColorType3(context),
+        margin: const EdgeInsets.symmetric(horizontal: 17),
+        shape: const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 17, horizontal: 21),
+          alignment: Alignment.centerLeft,
+          child: Text(
+            'Support',
+            style: Theme.of(context).textTheme.headline6,
+          ),
+        ),
+      ),
+    );
+  }
+}
