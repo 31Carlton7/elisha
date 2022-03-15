a859734bdf552435850c56d12359c8d1939f206a
Carlton Aikins <carltonaikins7@gmail.com>
2022-03-15 08:37:29 -0400

    Increment Build Number to 1.1.3 (13)

--- lib/src/config/constants.dart
+++ lib/src/config/constants.dart
@@ -20,7 +20,7 @@ import 'package:canton_design_system/canton_design_system.dart';
 
 const String kAppTitle = 'Elisha';
 
-const String kVersionNumber = 'v1.1.2 (12)';
+const String kVersionNumber = 'v1.1.3 (13)';
 
 const natureImages = <String>[
   'assets/images/nature_1.jpg',
